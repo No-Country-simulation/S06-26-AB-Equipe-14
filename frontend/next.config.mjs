882454d3ecca37fa9aa1ab7edc7034956dbf27c0
Frederico Macau <fredhymacau35@gmail.com>
2026-07-10 18:08:42 +0100

    fix(scripts): fix deployment script error in package.json

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,21 @@
-const backendUrl = process.env.BACKEND_INTERNAL_URL || 'http://localhost:8000';
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
     "10.148.224.205",
   ],
-  async rewrites() {
+};
+
+if (process.env.EXPORT_BUILD === 'true') {
+  nextConfig.output = 'export';
+} else {
+  const backendUrl = process.env.BACKEND_INTERNAL_URL || 'http://localhost:8000';
+  nextConfig.rewrites = async () => {
     return [
       {
         source: '/api/:path*',
         destination: `${backendUrl}/api/:path*`,
       },
     ];
-  },
-};
+  };
+}
 
 export default nextConfig;
